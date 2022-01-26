a218252a1d1ee151a07a27d70995bad496cf2c7b
Michael-August <michaelfrancis135@gmail.com>
2022-01-26 19:50:16 +0100

    Added the sign up and log in page

---
+++ src/app/auth-area/auth-area.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+const authRoutes: Routes = [
+   { path: 'signup', component: SignupComponent },
+   { path: 'login', component: LoginComponent }
+]
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(authRoutes)
+  ]
+})
+export class AuthAreaModule { }
